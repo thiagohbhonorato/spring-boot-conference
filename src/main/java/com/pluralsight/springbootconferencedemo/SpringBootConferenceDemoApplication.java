010726d74bc518c524b562dd85f427ca793bb905
Thiago Honorato <thiagohbhonorato@gmail.com>
2022-05-02 09:58:26 -0300

    Initial project created

---
+++ src/main/java/com/pluralsight/springbootconferencedemo/SpringBootConferenceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.springbootconferencedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConferenceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConferenceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluralsight/springbootconferencedemo/SpringBootConferenceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.springbootconferencedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConferenceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
